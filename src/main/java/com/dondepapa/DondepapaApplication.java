90346e3642ea9b36f47aa5e3136e06c41b742034
Juan Pablo Vindas Suarez <jvindas40886@ufide.ac.cr>
2024-03-07 15:49:47 -0600

    Proyecto Donde Papá Restaurant inicial

---
+++ src/main/java/com/dondepapa/DondepapaApplication.java
@@ -0,0 +1,13 @@
+package com.dondepapa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DondepapaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DondepapaApplication.class, args);
+	}
+
+}
